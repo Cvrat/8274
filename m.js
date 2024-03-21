4514f071b94e4fd515525c47e2c97d3bb76c3af3
Nadir <nadirmaharramov875@gmail.com>
2024-03-21 23:50:32 +0300

    9 commit

--- m.js
+++ m.js
@@ -136,3 +136,28 @@ function findMaxNumber(a , b , c , d ) {
 
 }
 console.log(findMaxNumber(1 , 15, 17 , 19 ))
+///////////////////////////////////////////////////////////////////////////////////////////
+const  po = {
+    a: 1234,
+    b: 'dajdaks',
+    c:{
+        colors:'FF00FF',
+        bg:'000000',
+        fmcolor:'00FF254',
+    }
+                                               //sabah otmetka qoyacam YADDIN CIXMASIN!!!
+};
+console.log(po.c , po.a  , po.b);
+console.log(po['c']['colors'])
+//delete po.b;
+console.log(po);
+for (let key in po){
+    if(typeof(po[key] === 'object')){
+     for(let i in po[key]){
+        console.log(`DoGa ${po.key} gosterir ${po[key][i]} `)
+     }
+    }else{
+        console.log(`DoGa ${key} gosterir ${po[key]} `);
+    }
+  
+};
